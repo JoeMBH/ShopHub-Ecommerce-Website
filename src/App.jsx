a699576435b6bf9e8a972584ed7681a55366b7c6
JoeMBH <joembhu@gmail.com>
2026-05-05 14:20:12 +0300

    Finished Cart and Checkout sections.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
-import Navbar from "./components/Navbar";
-import AuthProvider from "./context/AuthContext";
 import ProductDetails from "./pages/ProductDetails";
+
+import Navbar from "./components/Navbar";
+
+import AuthProvider from "./context/AuthContext";
+import CartProvider from "./context/CartContext";
 
 const App = () => {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
   );
 };
